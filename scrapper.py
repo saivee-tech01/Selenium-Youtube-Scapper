95ff169c4b253cba4f42bd5c76c2f2f324aaf6d3
Saivee Agrawal <agrawalsaivee@gmail.com>
2022-06-26 07:36:13 +0000

    First Commit

--- scrapper.py
+++ scrapper.py
@@ -1,1 +1,7 @@
-print('Hello World')
+import requests
+YOUTUBE_TRENDING_URL="https://www.youtube.com/feed/trending"
+response=requests.get(YOUTUBE_TRENDING_URL)
+print('STATUS CODE',response.status_code)
+#print('OUTPUT',response.text[:500])
+with open('trending.html','w') as f:
+  f.write(response.text)
