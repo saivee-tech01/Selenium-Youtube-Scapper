4d383c9ec87d590e9a0c45d8b6ec769a58d1ebe3
Saivee Agrawal <agrawalsaivee@gmail.com>
2022-06-26 20:37:42 +0000

    Second Commit

--- scrapper.py
+++ scrapper.py
@@ -1,7 +1,15 @@
 import requests
+from bs4 import BeautifulSoup
 YOUTUBE_TRENDING_URL="https://www.youtube.com/feed/trending"
 response=requests.get(YOUTUBE_TRENDING_URL)
 print('STATUS CODE',response.status_code)
 #print('OUTPUT',response.text[:500])
 with open('trending.html','w') as f:
   f.write(response.text)
+
+doc=BeautifulSoup(response.text,'html.parser')
+print('PageTitle',doc.title.text)
+
+#Find all video divs
+video_div=doc.find_all('div',class_='ytd-video-renderer')
+print(f'Found {len(video_div)} videos')
